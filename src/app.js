d860f2f778ee728deda9739ad89fdc23314c89fb
Carolina <carolina.peseca@gmail.com>
2025-04-21 11:20:31 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,50 @@
-import "bootstrap";
 import "./style.css";
+const whoList = [
+  "My dog",
+  "My sister",
+  "My grandma",
+  "My bird",
+  "My cousing",
+  "An alien",
+  "My neighbor",
+  "A ghost"
+];
+const actionList = [
+  "ate",
+  "peed",
+  "broke",
+  "cooked",
+  "set fire to",
+  "stole",
+  "hid",
+  "spilled hot chocolate on"
+];
+const whatList = [
+  "my homeworks",
+  "my laptop",
+  "the presentation",
+  "my code",
+  "my class project"
+];
+const whenList = [
+  "before the class",
+  "during breakfast",
+  "while I was sleeping",
+  "last night",
+  "while I was praying"
+];
 
+const _getRandomIndex = (max) =>{
+  const position = Math.floor(Math.random() * max);
+  return position;
+}
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+const _getRandomList = (list) =>{return list[_getRandomIndex(list.length)]}
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+const who = _getRandomList(whoList);
+const action = _getRandomList(actionList);
+const what = _getRandomList(whatList);
+const when = _getRandomList(whenList);
+
+const excuse = () => {return `${who} ${action} ${what} ${when}`}
+document.querySelector("#excuse").innerHTML = excuse();
